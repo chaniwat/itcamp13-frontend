27a03deace7e4697daa76286ffe9f38e876305e6
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2017-02-24 17:25:01 +0700

    Change bg color when slide in camp-block

--- public_html/assets/frontend/js/app.js
+++ public_html/assets/frontend/js/app.js
@@ -33549,29 +33549,40 @@ $(document).ready(() => {
   }
 
   registerOnSlideLeave(anchorLink, index, slideIndex, direction, nextSlideIndex) {
+    // Hide/Show the navigation
     if (nextSlideIndex == 0) {
       this.navigation.addClass('hide');
     } else {
       this.navigation.removeClass('hide');
     }
 
+    // Remove active & current camp slide class to block
+    this.block.removeClass((i, className) => {
+      return (className.match(/[a-zA-Z]+-camp/g) || []).join(' ');
+    });
     this.navigation.all.removeClass('active');
 
+    // Add active & current camp slide class to block
     switch (nextSlideIndex) {
       case 1:
         this.navigation.app.addClass('active');
+        this.block.addClass('app-camp');
         break;
       case 2:
         this.navigation.game.addClass('active');
+        this.block.addClass('game-camp');
         break;
       case 3:
         this.navigation.network.addClass('active');
+        this.block.addClass('network-camp');
         break;
       case 4:
         this.navigation.iot.addClass('active');
+        this.block.addClass('iot-camp');
         break;
       case 5:
         this.navigation.datasci.addClass('active');
+        this.block.addClass('datasci-camp');
         break;
     }
   }
--- src/main/block/camp/index.js
+++ src/main/block/camp/index.js
@@ -15,39 +15,50 @@ export class CampBlock {
     this.navigation.datasci = this.navigation.find('.datasci');
     this.navigation.all = this.navigation.find('li');
 
-    this.navigation.all.click(this.navigationOnClick)
+    this.navigation.all.click(this.navigationOnClick);
   }
 
   registerOnSlideLeave(anchorLink, index, slideIndex, direction, nextSlideIndex) {
+    // Hide/Show the navigation
     if(nextSlideIndex == 0) {
       this.navigation.addClass('hide');
     } else {
       this.navigation.removeClass('hide');
     }
 
+    // Remove active & current camp slide class to block
+    this.block.removeClass((i, className) => {
+        return (className.match(/[a-zA-Z]+-camp/g) || []).join(' ');
+    });
     this.navigation.all.removeClass('active');
 
+    // Add active & current camp slide class to block
     switch (nextSlideIndex) {
       case 1:
         this.navigation.app.addClass('active');
+        this.block.addClass('app-camp');
         break;
       case 2:
         this.navigation.game.addClass('active');
+        this.block.addClass('game-camp');
         break;
       case 3:
         this.navigation.network.addClass('active');
+        this.block.addClass('network-camp');
         break;
       case 4:
         this.navigation.iot.addClass('active');
+        this.block.addClass('iot-camp');
         break;
       case 5:
         this.navigation.datasci.addClass('active');
+        this.block.addClass('datasci-camp');
         break;
     }
   }
 
   navigationOnClick(event) {
-    let target = $(event.target).parent().data('target')
+    let target = $(event.target).parent().data('target');
     $.fn.fullpage.moveTo('camp-block', target);
   }
 
