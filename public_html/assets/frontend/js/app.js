e90c9e27e9af0f6189e1d0b552da42cfe7df0afb
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2017-03-11 03:14:58 +0700

    Finish landing page

--- public_html/assets/frontend/js/app.js
+++ public_html/assets/frontend/js/app.js
@@ -10670,14 +10670,29 @@ module.exports = __webpack_amd_options__;
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* WEBPACK VAR INJECTION */(function($) {Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 class LandingApp {
 
-  constructor() {}
+  constructor() {
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+      var ww = $(window).width() < window.screen.width ? $(window).width() : window.screen.width; //get proper width
+      var mw = 500; // min width of site
+      var ratio = ww / mw; //calculate ratio
+      if (ww < mw) {
+        //smaller than minimum size
+        $('#Viewport').attr('content', 'initial-scale=' + ratio + ', maximum-scale=' + ratio + ', minimum-scale=' + ratio + ', user-scalable=no, width=' + ww);
+      } else {
+        //regular size
+        $('#Viewport').attr('content', 'initial-scale=1.0, maximum-scale=2, minimum-scale=1.0, user-scalable=no, width=' + ww);
+      }
+    }
+  }
 
 }
 /* harmony export (immutable) */ __webpack_exports__["LandingApp"] = LandingApp;
 
+/* WEBPACK VAR INJECTION */}.call(__webpack_exports__, __webpack_require__(0)))
 
 /***/ }),
 /* 7 */
